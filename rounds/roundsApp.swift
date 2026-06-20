fa78be662e89c04ec6e09c61de4ce563e710f3d0
Michael Egorov <yaetiya@gmail.com>
2026-06-20 09:24:32 +0400

    Initial Commit

---
+++ rounds/roundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  roundsApp.swift
+//  rounds
+//
+//  Created by Michael Egorov on 6/20/26.
+//
+
+import SwiftUI
+
+@main
+struct roundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
